636ddfc8d594d5e355f44ca65d3a6a9eab45e614
SCSANDUSR101 <smaktech.dvm@gmail.com>
2023-03-31 08:24:30 +0100

    Updates

---
+++ static/js/155.baccfc06.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[155],{1699:function(i,a,t){"use strict";t.r(a);var m=t(2826);a.default=m.a}}]);
+//# sourceMappingURL=155.baccfc06.chunk.js.map
